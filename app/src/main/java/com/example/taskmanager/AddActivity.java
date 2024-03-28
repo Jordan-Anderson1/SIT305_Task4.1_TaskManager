2546f7f77b96226d12c75a3eb1c91b40f23e3b18
jordan <andersonjor@deakin.edu.au>
2024-03-28 22:33:02 +1100

    build database helper build basic wireframe and logic to add task Toast to show whether task already added

---
+++ app/src/main/java/com/example/taskmanager/AddActivity.java
@@ -0,0 +1,32 @@
+package com.example.taskmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+       EditText titleInput = findViewById(R.id.titleInput);
+       EditText descriptionInput = findViewById(R.id.descriptionInput);
+       Button submitButton = findViewById(R.id.submitButton);
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
+                myDB.addTodo(titleInput.getText().toString(), descriptionInput.getText().toString());
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/taskmanager/MyDatabaseHelper.java
@@ -0,0 +1,59 @@
+package com.example.taskmanager;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DATABASE_NAME = "todo.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NAME = "my_todos";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITLE = "todo_title";
+    private static final String COLUMN_DESCRIPTION = "todo_description";
+    private static final String COLUMN_COMPLETED = "todo_completed";
+
+    public MyDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME +
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_TITLE + " TEXT, " +
+                        COLUMN_DESCRIPTION + " TEXT, " +
+                        COLUMN_COMPLETED + " BOOLEAN);";
+
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+
+    }
+
+    void addTodo(String title, String description) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE, title);
+        cv.put(COLUMN_DESCRIPTION, description);
+        long result = db.insert(TABLE_NAME, null, cv);
+        if(result == -1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "Item added", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
