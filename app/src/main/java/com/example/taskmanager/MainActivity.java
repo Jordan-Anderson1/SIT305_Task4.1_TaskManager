6a2525e83079ec3b907add8d0bffd2bee0c26b58
jordan <andersonjor@deakin.edu.au>
2024-03-28 23:39:03 +1100

    set custom adapter and card view for main activity to display tasks

--- app/src/main/java/com/example/taskmanager/MainActivity.java
+++ app/src/main/java/com/example/taskmanager/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     FloatingActionButton addButton;
+
+    MyDatabaseHelper myDB;
+    ArrayList<String> todo_id, todo_title, todo_description;
+    ArrayList<Boolean> todo_completed;
+
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        myDB = new MyDatabaseHelper(MainActivity.this);
+        todo_id = new ArrayList<>();
+        todo_title = new ArrayList<>();
+        todo_description = new ArrayList<>();
+        todo_completed = new ArrayList<>();
 
+        storeDataInArrays();
+
+        customAdapter = new CustomAdapter(MainActivity.this, todo_id, todo_title, todo_description);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+    }
+
+    void storeDataInArrays(){
+        Cursor cursor = myDB.readAllData();
+        if(cursor.getCount() == 0){
+            Toast.makeText(this, "No data", Toast.LENGTH_SHORT).show();
+        }else{
+            while(cursor.moveToNext()){
+                todo_id.add(cursor.getString(0));
+                todo_title.add(cursor.getString(1));
+                todo_description.add(cursor.getString(2));
+
+                //get integer value, convert to boolean. Add bool value to list.
+                boolean completed = cursor.getInt(3) == 1;
+                todo_completed.add(completed);
+            }
+        }
     }
 }
