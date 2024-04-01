5bb7cce84527068034f4912dd7e36d0937eab883
jordan <andersonjor@deakin.edu.au>
2024-04-01 13:47:26 +1100

    go back to main screen after creatoing a todo

--- app/src/main/java/com/example/taskmanager/AddActivity.java
+++ app/src/main/java/com/example/taskmanager/AddActivity.java
@@ -2,6 +2,7 @@ package com.example.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +26,10 @@ public class AddActivity extends AppCompatActivity {
             public void onClick(View v) {
                 MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
                 myDB.addTodo(titleInput.getText().toString(), descriptionInput.getText().toString());
+
+                Intent intent = new Intent(AddActivity.this, MainActivity.class);
+                startActivity(intent);
+
             }
         });
 
